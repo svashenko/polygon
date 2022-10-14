0de7b3d2c2bb0dc4e47a80d9f6cf9b45bfe34800
svashenko <58010422+svashenko@users.noreply.github.com>
2022-10-14 19:13:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.reply-slider').slick({
+  infinite: true,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 800,
+  autoplay: false,
+  autoplaySpeed: 4000,
+  focusOnSelect: true,
+  pauseOnHover: true,
+  swipeToSlide: true,
+  nextArrow: '<div class="slick-arrow slick-next"><img src="img/arrow.svg" alt=""></span></div>',
+  prevArrow: '<div class="slick-arrow slick-prev"><img src="img/arrow.svg" alt=""></span></div>'
+});
+
